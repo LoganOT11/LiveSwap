18217c8fcd8a2cf51fd2815753e12b2bc9be54b5
mlane44 <mtplane03@gmail.com>
2025-11-21 23:14:15 -0500

    initial commit

---
+++ settings.py
@@ -0,0 +1,11 @@
+import os
+DEVICE_INDEX = 18         # Your VB-Cable Output ID
+SAMPLE_RATE = 44100       # Standard for video
+CHANNELS = 2              # Stereo is required for spatial detection
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# Now we build paths relative to the root
+RAW_RECORDINGS_DIR = os.path.join(BASE_DIR, "dataset", "raw_recordings")
+PROCESSED_DATASET_DIR = os.path.join(BASE_DIR, "dataset", "fast_1.0s")
+MODEL_PATH = os.path.join(BASE_DIR, "models", "ad_detector.pth")
